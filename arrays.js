8397d86cd8b0da362e513dbbd67a1bcf88379270
Jose Torres <vidojam@gmail.com>
2023-03-24 10:33:04 -0400

    did basic loops

---
+++ arrays.js
@@ -0,0 +1,33 @@
+// Given the following array, output each fruit
+
+const fruits = ['apple', 'banana', 'orange', 'kiwi', 'grapes']
+
+// write loop here...
+for (let i=0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+
+const students = [
+  {
+    firstName: 'Ilan',
+    lastName: 'Vaks',
+    gender: 'male',
+  },
+  {
+    firstName: 'Kaniel',
+    lastName: 'Tapper',
+    gender: 'male',
+  },
+  {
+    firstName: 'Meghann',
+    lastName: 'Stamper',
+    gender: 'female'
+  },
+]
+
+
+// Given the following array, output each student's first name
+for (i=0; i < students.length; i++) {
+  console.log(students[i].firstName);
+}
---
+++ basics.js
@@ -0,0 +1,16 @@
+// Write a loop to print "0. Hello" to "9. Hello" (10 lines)
+
+for(let i=0; i < 10; i++) {
+  console.log(`${i}. Hello`)
+}
+ 
+// Write a loop to count down from 10 to 1 (console.log)
+// and then write "Blast Off!"
+for(let i=10; i > 0; i--) {
+  console.log(i)
+}
+console.log("Blast Off!")
+
+// write loop here... 
+
+
